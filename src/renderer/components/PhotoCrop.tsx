bc1a5f6cba9ae16c1239ac44dcb333b4f2bf17eb
Ainis Taranda <taranda.ainis@gmail.com>
2022-12-01 09:39:58 -0500

    displaying image data

--- src/renderer/components/PhotoCrop.tsx
+++ src/renderer/components/PhotoCrop.tsx
@@ -1,13 +1,37 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { readFile } from '../../main/helpers';
 
 export default function PhotoCrop() {
-  return(
+  const [imageSrc, setImageSrc] = useState(null); // file data
+  const [filename, setFilename] = useState(null); // file address
+  const handleFileChange = async (e: any) => {
+    if(e.target.files && e.target.files.length) {
+      // we got a file...
+      const file = e.target.files[0]
+      setFilename(file.path)
+      // get the image data from the file...
+      const imageData: any = await readFile(file)
+      setImageSrc(imageData)
+    }
+  }
+  if (!imageSrc) {
+    return (
+      <>
+        <h1>Please choose a photo to crop</h1>
+        <input type="file" accept="image/*" onChange={handleFileChange} />
+        <button>
+          <Link className="small-btn" to="/">
+            {' '}
+            &lt; Back
+          </Link>
+        </button>
+      </>
+    );
+  }
+  return (
     <>
-    <h1>Please choose a photo to crop</h1>
-    <input type="file" accept="image/*" />
-    <button>
-    <Link className="small-btn" to="/"> &lt; Back</Link>
-    </button>
+      <img src={imageSrc} alt="" />
     </>
-  )
+  );
 }
