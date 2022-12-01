f881d308a8d5cd24b5bff1f4da22ead57e6697bb
Ainis Taranda <taranda.ainis@gmail.com>
2022-12-01 10:05:20 -0500

    cropper works but no save

--- src/renderer/components/PhotoCrop.tsx
+++ src/renderer/components/PhotoCrop.tsx
@@ -1,20 +1,24 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import Cropper from 'react-easy-crop';
 import { readFile } from '../../main/helpers';
 
 export default function PhotoCrop() {
   const [imageSrc, setImageSrc] = useState(null); // file data
   const [filename, setFilename] = useState(null); // file address
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+
   const handleFileChange = async (e: any) => {
-    if(e.target.files && e.target.files.length) {
+    if (e.target.files && e.target.files.length) {
       // we got a file...
-      const file = e.target.files[0]
-      setFilename(file.path)
+      const file = e.target.files[0];
+      setFilename(file.path);
       // get the image data from the file...
-      const imageData: any = await readFile(file)
-      setImageSrc(imageData)
+      const imageData: any = await readFile(file);
+      setImageSrc(imageData);
     }
-  }
+  };
   if (!imageSrc) {
     return (
       <>
@@ -31,7 +35,11 @@ export default function PhotoCrop() {
   }
   return (
     <>
-      <img src={imageSrc} alt="" />
+      <Cropper image={imageSrc} 
+      crop={crop}
+      zoom={zoom}
+      onCropChange={setCrop}
+      onZoomChange={setZoom} />
     </>
   );
 }
